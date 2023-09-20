2efca55b153baba826a9db797fe2ad5c65f5230e
Dominik Meyer <dominik.meyer@plentymarkets.com>
2023-09-20 14:50:38 +0200

    Update index.ts

--- apps/web/sdk/index.ts
+++ apps/web/sdk/index.ts
@@ -5,7 +5,7 @@ import { createSharedComposable } from '@vueuse/core';
 export const useSdk = createSharedComposable(() => {
   const sdkConfig = {
     plentysystems: buildModule<PlentysystemsModuleType>(plentysystemsModule, {
-      apiUrl: 'http://localhost:8181/plentysystems',
+      apiUrl: 'https://ceres-demo.plentymarkets-cloud01.com/plentysystems',
     }),
   };
 
