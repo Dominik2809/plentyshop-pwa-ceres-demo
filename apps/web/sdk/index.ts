31600d058422352a672cfaf49851ac05f62c246c
ksted <kevin.stederoth@plentymarkets.com>
2023-09-26 10:11:40 +0200

    chore: test with interceptor

--- apps/web/sdk/index.ts
+++ apps/web/sdk/index.ts
@@ -1,15 +1,32 @@
-import { PlentysystemsModuleType, plentysystemsModule } from '@plentymarkets/shop-sdk';
+import { PlentysystemsModuleType, plentysystemsModule, client } from '@plentymarkets/shop-sdk';
 import { initSDK, buildModule } from '@vue-storefront/sdk';
 import { createSharedComposable } from '@vueuse/core';
 
 export const useSdk = createSharedComposable(() => {
   const config = useRuntimeConfig();
-  
+  const cookieHeaders = useRequestHeaders(['cookie']);
+
   const sdkConfig = {
     plentysystems: buildModule<PlentysystemsModuleType>(plentysystemsModule, {
       apiUrl: `${config.public.apiUrl}/plentysystems`,
     }),
   };
+
+  client.interceptors.request.use(
+    (config) => {
+      if (process.server) {
+        if (!config.headers) {
+          config.headers = {};
+        }
+        config.headers.cookie = cookieHeaders.cookie ?? '';
+      }
+      return config;
+    },
+    (error) => {
+      // Do something with request error
+      return Promise.reject(error);
+    },
+  );
 
   return initSDK<typeof sdkConfig>(sdkConfig);
 });
